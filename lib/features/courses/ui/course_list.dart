8ba1f282a11d5ec5715951b0c1e74c7d798ecb2c
AtharvaParadkar <atharvaparadkar91@gmail.com>
2024-10-13 13:29:13 +0530

    Added Bloc

---
+++ lib/features/courses/ui/course_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CourseLisrPage extends StatefulWidget {
+  const CourseLisrPage({super.key});
+
+  @override
+  State<CourseLisrPage> createState() => _CourseLisrPageState();
+}
+
+class _CourseLisrPageState extends State<CourseLisrPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       title: 'Course App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.cyan,
       ),
       home: const Scaffold(),
     );
